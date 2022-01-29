5e161dca98b9f51e68ad5d57dc289e8ef5a28f8a
Aapo Kiviniemi <93884246+AapoStone@users.noreply.github.com>
2022-01-30 01:30:37 +0200

    Html-runko

--- src/App.js
+++ src/App.js
@@ -2,7 +2,49 @@ import './App.css';
 
 function App() {
   return (
+    <from>
+    <h1>Calculating alcohol blood level</h1>
     
+    <div>
+      <label>Weight</label>
+
+      <input 
+      type="number"
+      value="" 
+      />
+    </div>
+
+    <div>
+      <label>Bottles</label>
+      
+      <input 
+      type="number"
+      value="" 
+      />
+    </div>
+
+    <div>
+      <label>Time</label>
+      
+      <input 
+      type="number"
+      value="" 
+      />
+    </div>
+
+    <div>
+    <label>Gender</label>
+    <input type="radio" value="Male" name="gender" /> Male
+    <input type="radio" value="Female" name="gender" /> Female
+    </div>
+
+
+    </from>
+
+
+
+
+
   );
 }
 
