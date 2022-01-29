585ce7034804ac9f232e0f8d1a9cbbb3be3da14d
Aapo Kiviniemi <93884246+AapoStone@users.noreply.github.com>
2022-01-30 01:43:36 +0200

    Const(s), calculate button ja answer output

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+const [weight,setWeight] = useState(0);
+const [bottles,setBottles] = useState(0);
+const [time,setTime] = useState(0);
+
+
+
+
+
   return (
     <from>
     <h1>Calculating alcohol blood level</h1>
@@ -37,8 +47,13 @@ function App() {
     <input type="radio" value="Male" name="gender" /> Male
     <input type="radio" value="Female" name="gender" /> Female
     </div>
-
-
+    <div className='answer'>
+      <label>Answer</label>
+      <output> </output>
+    </div>
+    <div className='button'>
+    <button>Calculate</button>
+    </div>
     </from>
 
 
