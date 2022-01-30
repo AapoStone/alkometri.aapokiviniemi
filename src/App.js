34b1a87cd20f62921c6f19ee88258eb408c92851
Aapo Kiviniemi <93884246+AapoStone@users.noreply.github.com>
2022-01-30 02:03:34 +0200

    Bottles/Time muutto dropdownlist+gender korjaus

--- src/App.js
+++ src/App.js
@@ -26,26 +26,69 @@ const [time,setTime] = useState(0);
 
     <div>
       <label>Bottles</label>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+      </select>
       
-      <input 
-      type="number"
-      value="" 
-      />
     </div>
 
     <div>
       <label>Time</label>
-      
-      <input 
-      type="number"
-      value="" 
-      />
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+      </select>
     </div>
 
     <div>
     <label>Gender</label>
-    <input type="radio" value="Male" name="gender" /> Male
-    <input type="radio" value="Female" name="gender" /> Female
+    <input type="radio" value="male" name="gender" defaultChecked /> Male
+    <input type="radio" value="female" name="gender" /> Female
     </div>
     <div className='answer'>
       <label>Answer</label>
